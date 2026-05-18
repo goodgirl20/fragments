fe60e2edc7d2ef883311449a1a26ec79251bb9c2
Grace <grace.sally.thomas9656@gmail.com>
2026-05-18 13:05:15 -0400

    Fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+];
